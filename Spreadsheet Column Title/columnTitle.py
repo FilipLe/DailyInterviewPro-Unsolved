2feb11c991e28358711662713e89b41bb2f9590a
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-06 12:08:27 +0200

    Create columnTitle.py

---
+++ Spreadsheet Column Title/columnTitle.py
@@ -0,0 +1,13 @@
+class Solution:
+  def convertToTitle(self, n):
+    # Fill this in.
+
+input1 = 1
+input2 = 456976
+input3 = 28
+print(Solution().convertToTitle(input1))
+# A
+print(Solution().convertToTitle(input2))
+# YYYZ
+print(Solution().convertToTitle(input3))
+# AB
