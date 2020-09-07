a79ec871d5a4ef44395e49398debca379753dbd8
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-07 10:37:20 +0200

    Update checkPalindrome.py

--- Palindrome Integers (SOLVED)/checkPalindrome.py
+++ Palindrome Integers (SOLVED)/checkPalindrome.py
@@ -4,15 +4,20 @@ def is_palindrome(n):
     # Fill this in.
     #Convert num into string
     stringVal = str(n)
+    
     #Store the digits in a list
     arr = []
+    
     #Loop through the digits and append them into the list arr[]
     for digits in stringVal:
         arr.append(digits)
+        
     #Number of the digits
     sizeVal = len(arr)
+    
     #initialize counter
     counter = 0
+    
     #since we're working with palindromes supposingly, we only 
     #need to loop through the first half and check if they 
     #correspond to the last half
@@ -28,4 +33,4 @@ print(is_palindrome(1234321))
 print(is_palindrome(1234322))
 # False
 print(is_palindrome(1221))
-# False
+# True
