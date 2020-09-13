8c8d8d7d6a7656faa2bc837b5c24cd2c646a53b7
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-13 14:37:34 +0200

    Create starterCode.py

---
+++ Queens on a chessboard/starterCode.py
@@ -0,0 +1,12 @@
+def n_queens(n):
+  # Fill this in.
+
+print(n_queens(5))
+# There can be many answers
+# [(0, 0), (1, 2), (2, 4), (3, 1), (4, 3)]
+
+# Q . . . .
+# . . . Q .
+# . Q . . .
+# . . . . Q
+# . . Q . .
