0905e94dc539398e2b1d27db18c961d497f11137
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-14 09:27:46 +0200

    Update starterCode.py

--- Split a Binary Search Tree/starterCode.py
+++ Split a Binary Search Tree/starterCode.py
@@ -1,1 +1,43 @@
+class Node:
+  def __init__(self, value, left=None, right=None):
+    self.value = value
+    self.left = left
+    self.right = right
 
+  def __repr__(self):
+    if self.left and self.right:
+      return f"({self.value}, {self.left}, {self.right})"
+    if self.left:
+      return f"({self.value}, {self.left})"
+    if self.right:
+      return f"({self.value}, None, {self.right})"
+    return f"({self.value})"
+
+def split_bst(bst, s):
+  # Fill this in.
+  
+n2 = Node(2)
+n1 = Node(1, None, n2)
+
+n5 = Node(5)
+n4 = Node(4, None, n5)
+
+root = Node(3, n1, n4)
+
+print(root)
+# (3, (1, (2)), (4, None, (5)))
+# How the tree looks like
+#     3
+#   /   \
+#  1     4
+#   \     \
+#    2     5
+
+print(split_bst(root, 2))
+# ((1, None, (2)), (3, None, (4, None, (5))))
+# Split into two trees
+# 1    And   3
+#  \          \
+#   2          4
+#               \
+#                5
