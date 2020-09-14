f0ad81fe6e08c77dc21fea251cf42ad24bc41c74
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-14 15:14:54 +0200

    Create starterCode.py

---
+++ Remove One Layer of Parenthesis/starterCode.py
@@ -0,0 +1,11 @@
+def remove_outermost_parenthesis(s):
+  # Fill this in.
+
+print(remove_outermost_parenthesis('(())()'))
+# ()
+
+print(remove_outermost_parenthesis('(()())'))
+# ()()
+
+print(remove_outermost_parenthesis('()()()'))
+# ' '
