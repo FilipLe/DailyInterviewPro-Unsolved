33eaabe0630def93e48bd7571700c28571cac487
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-14 09:16:53 +0200

    Update fixedPoints.py

--- Fixed Point (SOLVED)/fixedPoints.py
+++ Fixed Point (SOLVED)/fixedPoints.py
@@ -3,6 +3,7 @@ def find_fixed_point(nums):
     counter = 0
     arr = [] #array to store the fixed points
     while counter < len(nums):
+        #if index is equal to number at that index, store it in arr 
         if counter == nums[counter]:
             arr.append(counter)
         counter += 1
