018ef9cdb0e8ea03716776397c95520bd0a5faad
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-14 09:49:34 +0200

    Update starterCode.py

--- Nearest Points/starterCode.py
+++ Nearest Points/starterCode.py
@@ -1,1 +1,19 @@
+class Point:
+  def __init__(self, x=0, y=0):
+    self.x = x
+    self.y = y
 
+  def __repr__(self):
+    return f"({self.x}, {self.y})"
+
+def closest_points(points, k, p):
+  # Fill this in.
+
+points = [
+    Point(0, 0),
+    Point(1, 1),
+    Point(2, 2),
+    Point(3, 3),
+]
+print(closest_points(points, 2, Point(0, 2)))
+# [(0, 0), (1, 1)]
