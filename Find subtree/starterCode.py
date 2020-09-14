5c8811c3a6a7de38ac0f746f649ea90b0c5a073c
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-14 09:41:20 +0200

    Update starterCode.py

--- Find subtree/starterCode.py
+++ Find subtree/starterCode.py
@@ -1,1 +1,33 @@
+class Node:
+  def __init__(self, value, left=None, right=None):
+    self.value = value
+    self.left = left
+    self.right = right
 
+  def __repr__(self):
+    return f"(Value: {self.value} Left: {self.left} Right: {self.right})"
+
+def find_subtree(s, t):
+  # Fill this in.
+
+t3 = Node(4, Node(3), Node(2))
+t2 = Node(5, Node(4), Node(-1))
+t = Node(1, t2, t3)
+
+s = Node(4, Node(3), Node(2))
+"""
+Tree t:
+    1
+   / \
+  4   5 
+ / \ / \
+3  2 4 -1
+
+Tree s:
+  4 
+ / \
+3  2 
+"""
+
+print(find_subtree(s, t))
+# True
