4898032a91b1b0376d09c8356daf7410404c3663
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-17 15:22:48 +0200

    Create starterCode.py

---
+++ Number of Meeting Rooms/starterCode.py
@@ -0,0 +1,9 @@
+def meeting_rooms(meetings):
+  # Fill this in.
+
+# print 1
+print(meeting_rooms([(0, 10), (10, 20)]))
+# 1
+
+print(meeting_rooms([(20, 30), (10, 21), (0, 50)]))
+# 3 (all meetings overlap at time 20)
