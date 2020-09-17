8200dab25c6432a83a58e5fe9793666c2d8bfe2d
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2020-09-17 15:19:06 +0200

    Create starterCode.py

---
+++ Remove Character to Create Palindrome/starterCode.py
@@ -0,0 +1,9 @@
+def create_palindrome(s):
+  # Fill this in.
+
+print(create_palindrome("abcdcbea"))
+# True
+print(create_palindrome("abccba"))
+# False
+print(create_palindrome("abccaa"))
+# False
