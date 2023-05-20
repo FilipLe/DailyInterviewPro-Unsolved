cd60e367a2f0f10512f18df536c8dbdba80d05d3
Nguyen Le <48429871+FilipLe@users.noreply.github.com>
2023-05-20 12:24:25 +0700

    Completed powerFunc.py

--- Power Function/powerFunc.py
+++ Power Function/powerFunc.py
@@ -1,5 +1,14 @@
 def pow(x, n):
-  # Fill this in.
+    # base case
+    if n == 0:
+        return 1
+    
+    #recursive case
+    else:
+        if x % 2 == 0:
+            return pow(x, n/2)
+        else:
+            return x * pow(x, n - 1)
 
 print(pow(5, 3))
 # 125
